0ff724965416a5f2f3c0b20d49aadc4e8177368f
Аnna Lizunova <yashikova@yandex.ru>
2025-01-10 14:35:41 +0300

    PEP8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +9,6 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 ALLOWED_HOSTS = ['89.169.164.157', '127.0.0.1', 'localhost', 'shikovanya.zapto.org']
-
 
 
 # Application definition
@@ -73,7 +72,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
 
 
 # Password validation
